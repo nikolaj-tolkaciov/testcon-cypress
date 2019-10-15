7b8c8ee74699e67167b8fed10c4c44b9195e1bb7
staaiv0 <aivaras.stabingis@saiglobal.com>
2019-10-15 12:42:14 +0300

    verify todays date

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -7,11 +7,11 @@ describe('Login functionality', function() {
         cy.get('.Select.not-valid').should('be.visible')
     })
 
-    it('Should be able to login with role User', function () {
+    it('Should be able to login with role team lead', function () {
         cy.get('[id="loginForm.userId"]').click({force:true})
         cy.get('[aria-label="Demo User"]').click()
         cy.get('[id="loginForm.role"]').click({force:true})
-        cy.get('[aria-label="User"]').click()
+        cy.get('[aria-label="Team Lead"]').click()
         cy.get('[type="submit"]').click()
 
         cy.url().should('include', '/time-logging')
@@ -19,6 +19,10 @@ describe('Login functionality', function() {
         cy.get('.calendar').should('be.visible')
         cy.get('.tile.form').should('be.visible')
         cy.get('.user-info__title').contains('Demo User')
-        cy.get('.main-nav').find('li').should('have.length', 1)
+        cy.get('.main-nav').find('li').should('have.length', 2)
+
+	it('Should verify todays date')
+        cy.get('.calendar--selected').contains(`${new Date().getDate()}`)
+		
     })
 })
