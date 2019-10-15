2e082c882425d0581aca84b9b7f4f4932d2b9fcc
Kseniia Stativka <kstativka@clincase.com>
2019-10-15 12:29:06 +0300

    Check current date

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -21,6 +21,6 @@ describe("Login functionality", function() {
     cy.get(".main-nav")
       .find("li")
       .should("have.length", 2);
-    cy.get("");
+    cy.get(".calendar--today").contains(`${new Date().getDate()}`);
   });
 });
