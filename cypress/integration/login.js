8af1a8ce1da56d17b25e6392343330388adfd89a
angproft <angproft@publicisgroupe.net>
2019-10-15 16:11:46 +0300

    Created test case to verify that login with all user roles is possible and correct number of tabs are shown

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -24,4 +24,47 @@ describe('Login functionality', function() {
         var day = date.getDate();
         cy.get('.calendar--today').contains(day)
     })
+
+    it('Should be able to login with all user roles', function () {
+        var users = 
+        [{
+            userRole: 'User',
+            numTabs: 1
+        },
+        {
+            userRole: 'Team Lead',
+            numTabs: 2
+        },
+        {
+            userRole: 'Manager',
+            numTabs: 5
+        },
+        {
+            userRole: 'Accountant',
+            numTabs: 5
+        },
+        {
+            userRole: 'Admin',
+            numTabs: 6
+        }]
+
+        users.forEach(user => {
+            cy.visit('/')
+            cy.get('[id="loginForm.userId"]').click({force:true})
+            cy.get('[aria-label="TestCon User 3"]').click()
+            cy.get('[id="loginForm.role"]').click({force:true})
+            cy.get(`[aria-label="${user.userRole}"]`).click()
+            cy.get('[type="submit"]').click()
+            cy.url().should('include', '/time-logging')
+
+            cy.get('.user-info__title').contains('TestCon User 3')
+            cy.get('.main-nav').find('li').should('have.length', `${user.numTabs}`)
+
+            cy.get('[aria-labelledby="timeLoggingId"]').should('have.css', 'color', 'rgb(64, 76, 237)')
+
+            cy.get('.user-button').click()
+            cy.get('button').contains('Log Out').click()
+        });
+    })
+
 })
