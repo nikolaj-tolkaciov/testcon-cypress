807d9c04f0d5e97f0e4c7cf33dc78294bb7102b0
Davin Gauthier <anonymi@MacBook-Pro.local>
2019-10-15 11:26:18 +0300

    first commit

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -4,10 +4,10 @@ const USER_ROLES_MAP = {
     "Manager": "role=managers",
     "Accountant": "role=accountants",
     "Admin": "role=admins"
-}
+};
 
 Cypress.Commands.add('loginAs', (role) => {
-    window.localStorage.setItem('USER_INFO', '{"id":1,"firstName":"Demo","lastName":"User"}')
+    window.localStorage.setItem('USER_INFO', '{"id":1,"firstName":"Demo","lastName":"User"}');
     window.localStorage.setItem('ACCESS_TOKEN', `userId=1&${USER_ROLES_MAP[role]}`)
-})
+});
 
