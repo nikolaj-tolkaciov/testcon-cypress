bfd84d1e6aa3cfeb252228e0c1b822a54689d81d
Kurt Duyck <kurt.duyck@vives.be>
2019-10-15 14:45:07 +0200

    Loop all Roles using DataCollection

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -7,6 +7,48 @@ describe('Login functionality', function() {
         cy.get('.Select.not-valid').should('be.visible')
     })
 
+    const testUser = 'TestCon User 24'
+
+    const expectedDataCollection = [
+        {
+            name: 'User',
+            navTabs: 1
+        },
+        {
+            name: 'Team Lead',
+            navTabs: 2
+        },
+        {
+            name: 'Manager',
+            navTabs: 5
+        },
+        {
+            name: 'Accountant',
+            navTabs: 5
+        },
+        {
+            name: 'Admin',
+            navTabs: 6
+        }
+    ]
+
+    expectedDataCollection.forEach(expectedData => {
+        it(`Should log in with ${expectedData.name} role and have ${expectedData.navTabs} tabs in navigation`, () => {
+            cy.visit('/')
+            cy.get('[id="loginForm.role"]')
+            cy.get('[id="loginForm.userId"]').click({ force: true })
+            cy.get(`[aria-label="${testUser}"]`).click()
+            cy.get('[id="loginForm.role"]').click({ force: true })
+            cy.get(`[aria-label="${expectedData.name}"]`).click()
+            cy.get('[type="submit"]').click()
+            cy.url().should('include', '/time-logging')
+            cy.get('.main-nav').find('li').should('have.length', expectedData.navTabs)
+            cy.get('.main-nav__link--active').contains('Time Logging')
+        })
+    })
+})
+
+/*
     it('Should be able to login with role User', function () {
         cy.get('[id="loginForm.userId"]').click({force:true})
         cy.get('[aria-label="TestCon User 24"]').click()
@@ -28,4 +70,4 @@ describe('Login functionality', function() {
         let d = new Date(), a = d.getUTCDate();
         cy.get('.calendar--today').contains(a);
     })
-})
+})*/
