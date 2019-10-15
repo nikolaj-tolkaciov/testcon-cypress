207ce48f0bf9b227023cda6f3001ac6967286968
Kseniia Stativka <kstativka@clincase.com>
2019-10-15 15:30:51 +0300

    Add test to check that all user can login and have correct number of tabs

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -23,4 +23,48 @@ describe("Login functionality", function() {
       .should("have.length", 2);
     cy.get(".calendar--today").contains(`${new Date().getDate()}`);
   });
+
+  const rolesData = [
+    {
+      name: "User",
+      tabs: 1
+    },
+    {
+      name: "Team Lead",
+      tabs: 2
+    },
+    {
+      name: "Manager",
+      tabs: 5
+    },
+    {
+      name: "Accountant",
+      tabs: 5
+    },
+    {
+      name: "Admin",
+      tabs: 6
+    }
+  ];
+
+  rolesData.forEach(roles => {
+    it(`Should login as ${roles.name} and have ${roles.tabs} tabs`, function() {
+      cy.clearLocalStorage("ACCESS_TOKEN");
+      cy.reload();
+      cy.get('[id="loginForm.userId"]').click({ force: true });
+      cy.get('[aria-label="TestCon User 2"]').click();
+      cy.get('[id="loginForm.role"]').click({ force: true });
+      cy.get(`[aria-label="${roles.name}"]`).click();
+      cy.get('[type="submit"]').click();
+
+      cy.get(".user-info__title").contains("TestCon User 2");
+      cy.get(".main-nav")
+        .find("li")
+        .should("have.length", roles.tabs);
+      cy.get(".main-nav__link--active").contains("Time Logging");
+      cy.get(".main-nav__link--active")
+        .should("have.css", "color")
+        .and("equal", "rgb(64, 76, 237)");
+    });
+  });
 });
