af7bf83cbf100cd2e46530ebce094f0b3e04465a
Julius Navi <navjulius@gmail.com>
2019-10-15 12:32:35 +0300

    first commit

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -1,24 +1,48 @@
-describe('Login functionality', function() {
-    it('Should display validation for empty user after attempted loggin', function () {
+// describe('Login functionality', function() {
+//     it('Should display validation for empty user after attempted loggin', function () {
        
+//         cy.visit('/')
+//         cy.get('.Select.not-valid').should('not.visible')
+//         cy.get('[type="submit"]').click()
+//         cy.get('.Select.not-valid').should('be.visible')
+//     })
+
+//     it('Should be able to login with role User', function () {
+//         cy.get('[id="loginForm.userId"]').click({force:true})
+//         cy.get('[aria-label="Demo User"]').click()
+//         cy.get('[id="loginForm.role"]').click({force:true})
+//         cy.get('[aria-label="User"]').click()
+//         cy.get('[type="submit"]').click()
+
+//         cy.url().should('include', '/time-logging')
+//         cy.get('.page__title').contains('Timesheets')
+//         cy.get('.calendar').should('be.visible')
+//         cy.get('.tile.form').should('be.visible')
+//         cy.get('.user-info__title').contains('Demo User')
+//         cy.get('.main-nav').find('li').should('have.length', 1)
+//     });
+// })
+
+describe('Login functionality 2', function() {
+    it('Should display validation for empty user after attempted loggin', () =>{
         cy.visit('/')
         cy.get('.Select.not-valid').should('not.visible')
         cy.get('[type="submit"]').click()
         cy.get('.Select.not-valid').should('be.visible')
-    })
-
-    it('Should be able to login with role User', function () {
+    });
+    it('Verify all user roles can log in and should see appropriate tabs', ()=>{
         cy.get('[id="loginForm.userId"]').click({force:true})
-        cy.get('[aria-label="Demo User"]').click()
+        cy.get('[aria-label="TestCon User 1"]').click()
         cy.get('[id="loginForm.role"]').click({force:true})
-        cy.get('[aria-label="User"]').click()
+        cy.get('[aria-label="Team Lead"]').click()
         cy.get('[type="submit"]').click()
 
         cy.url().should('include', '/time-logging')
         cy.get('.page__title').contains('Timesheets')
         cy.get('.calendar').should('be.visible')
         cy.get('.tile.form').should('be.visible')
-        cy.get('.user-info__title').contains('Demo User')
-        cy.get('.main-nav').find('li').should('have.length', 1)
+        cy.get('.user-info__title').contains('TestCon User 1')
+        cy.get('.main-nav').find('li').should('have.length', 2)
+        cy.get('.calendar--today').contains('15')
     })
 })
