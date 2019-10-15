3af3b607c014b9a01f2dcb653850f05ce3454db9
Egle7 <ean@solutionlab.net>
2019-10-15 15:17:50 +0300

    verify all user roles

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -1,5 +1,8 @@
 describe('Login functionality', function() {
-    it('Should display validation for empty user after attempted loggin', function () {
+
+
+      beforeEach( function(){
+    //it('Should display validation for empty user after attempted loggin', function () {
        
         cy.visit('/')
         cy.get('.Select.not-valid').should('not.visible')
@@ -7,9 +10,21 @@ describe('Login functionality', function() {
         cy.get('.Select.not-valid').should('be.visible')
     })
 
-    it('Should be able to login with role User', function () {
+    afterEach( function () {
+        cy.get('.user-info__title').click()
+        try{
+            cy.get('.btn__list-item').contains('Log Out').click()
+            
+        } catch(e) {
+            
+        }
+
+      })
+
+     it('Should be able to login with role User', function () {
+  
         cy.get('[id="loginForm.userId"]').click({force:true})
-        cy.get('[aria-label="Demo User"]').click()
+        cy.get('[aria-label="TestCon User 5"]').click()
         cy.get('[id="loginForm.role"]').click({force:true})
         cy.get('[aria-label="User"]').click()
         cy.get('[type="submit"]').click()
@@ -18,8 +33,70 @@ describe('Login functionality', function() {
         cy.get('.page__title').contains('Timesheets')
         cy.get('.calendar').should('be.visible')
         cy.get('.tile.form').should('be.visible')
-        cy.get('.user-info__title').contains('Demo User')
+        cy.get('.user-info__title').contains('TestCon User 5')
         cy.get('.main-nav').find('li').should('have.length', 1)
+        cy.get('.calendar--today').contains(`${new Date().getDate()}`)
+    })
+
+    it('Should be able to login with role Team Lead', function () {
+        cy.get('[id="loginForm.userId"]').click({force:true})
+        cy.get('[aria-label="TestCon User 5"]').click()
+        cy.get('[id="loginForm.role"]').click({force:true})
+        cy.get('[aria-label="Team Lead"]').click()
+        cy.get('[type="submit"]').click()
+
+        cy.url().should('include', '/time-logging')
+        cy.get('.page__title').contains('Timesheets')
+        cy.get('.calendar').should('be.visible')
+        cy.get('.tile.form').should('be.visible')
+        cy.get('.user-info__title').contains('TestCon User 5')
+        cy.get('.main-nav').find('li').should('have.length', 2)
+        cy.get('.calendar--today').contains(`${new Date().getDate()}`)
+    })
+
+    it('Should be able to login with role Manager', function () {
+        cy.get('[id="loginForm.userId"]').click({force:true})
+        cy.get('[aria-label="TestCon User 5"]').click()
+        cy.get('[id="loginForm.role"]').click({force:true})
+        cy.get('[aria-label="Manager"]').click()
+        cy.get('[type="submit"]').click()
+
+        cy.url().should('include', '/time-logging')
+        cy.get('.page__title').contains('Timesheets')
+        cy.get('.calendar').should('be.visible')
+        cy.get('.tile.form').should('be.visible')
+        cy.get('.user-info__title').contains('TestCon User 5')
+        cy.get('.main-nav').find('li').should('have.length', 5)
+        cy.get('.calendar--today').contains(`${new Date().getDate()}`)
+    })
+    it('Should be able to login with role Accountant', function () {
+        cy.get('[id="loginForm.userId"]').click({force:true})
+        cy.get('[aria-label="TestCon User 5"]').click()
+        cy.get('[id="loginForm.role"]').click({force:true})
+        cy.get('[aria-label="Accountant"]').click()
+        cy.get('[type="submit"]').click()
+
+        cy.url().should('include', '/time-logging')
+        cy.get('.page__title').contains('Timesheets')
+        cy.get('.calendar').should('be.visible')
+        cy.get('.tile.form').should('be.visible')
+        cy.get('.user-info__title').contains('TestCon User 5')
+        cy.get('.main-nav').find('li').should('have.length', 5)
+        cy.get('.calendar--today').contains(`${new Date().getDate()}`)
+    })
+    it('Should be able to login with role Admin', function () {
+        cy.get('[id="loginForm.userId"]').click({force:true})
+        cy.get('[aria-label="TestCon User 5"]').click()
+        cy.get('[id="loginForm.role"]').click({force:true})
+        cy.get('[aria-label="Admin"]').click()
+        cy.get('[type="submit"]').click()
+
+        cy.url().should('include', '/time-logging')
+        cy.get('.page__title').contains('Timesheets')
+        cy.get('.calendar').should('be.visible')
+        cy.get('.tile.form').should('be.visible')
+        cy.get('.user-info__title').contains('TestCon User 5')
+        cy.get('.main-nav').find('li').should('have.length', 6)
         cy.get('.calendar--today').contains(`${new Date().getDate()}`)
     })
 })
