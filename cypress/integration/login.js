ccb60c2e8d0adfeb2266227c21b1f5ef1410547f
angproft <angproft@publicisgroupe.net>
2019-10-15 12:42:42 +0300

    Get current date and verify if it's selected in calendar and changed locator

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -20,6 +20,8 @@ describe('Login functionality', function() {
         cy.get('.tile.form').should('be.visible')
         cy.get('.user-info__title').contains('Demo User')
         cy.get('.main-nav').find('li').should('have.length', 2)
-        cy.get('[data-test="timesheets-current-day"]').contains('15')
+        var date = new Date();
+        var day = date.getDate();
+        cy.get('.calendar--today').contains(day)
     })
 })
