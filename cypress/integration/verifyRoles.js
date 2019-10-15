4e4b4ff8c75a46ec01e9e47f1491e12341a4480a
asnejuste <as.ne.juste@gmail.com>
2019-10-15 15:34:11 +0300

    Used array to loop

--- cypress/integration/verifyRoles.js
+++ cypress/integration/verifyRoles.js
@@ -7,26 +7,34 @@ describe('T2 Verify Roles', function() {
         cy.get('.Select.not-valid').should('be.visible')
     })
 
-    let rolesArray = ["User", "Team Lead", "Manager", "Accountant", "Admin"];
+    let rolesArray = [{"role":"User", "tabs": 1},{"role":"Team Lead", "tabs": 2},{"role":"Manager", "tabs": 5},{"role":"Accountant", "tabs": 5},{"role":"Admin", "tabs": 6}];
 
-    it('Should be able to login with role User', function () {
-        cy.get('[id="loginForm.userId"]').click({force:true})
-        cy.get('[aria-label="TestCon User 6"]').click()
-        cy.get('[id="loginForm.role"]').click({force:true})
-        cy.get('[aria-label="User"]').click()
-        cy.get('[type="submit"]').click()
+    rolesArray.forEach(item => {
+        it('Should be able to login with role ' + item.role, function () {
+            console.log(item.role);
+            console.log(item.tabs);
+            
+            cy.get('[id="loginForm.userId"]').click({force:true})
+            cy.get('[aria-label="TestCon User 6"]').click()
+            cy.get('[id="loginForm.role"]').click({force:true})
+            cy.get(`[aria-label="${item.role}"]`).click()
 
-        cy.url().should('include', '/time-logging')
-        cy.get('.page__title').contains('Timesheets')
-        cy.get('.calendar').should('be.visible')
-        cy.get('.tile.form').should('be.visible')
-        cy.get('.user-info__title').contains('TestCon User 6')
-        cy.get('.main-nav').find('li').should('have.length', 1)
+    
+            cy.get('[type="submit"]').click()
+    
+            cy.url().should('include', '/time-logging')
+            //cy.get('.page__title').contains('Timesheets')
+            cy.get('.calendar').should('be.visible')
+            cy.get('.tile.form').should('be.visible')
+            cy.get('.user-info__title').contains('TestCon User 6')
+            cy.get('.main-nav').find('li').should('have.length', item.tabs)
+    
+            cy.get('.main-nav__link--active').contains('Time Logging');
+            cy.get('.main-nav__link--active').should('have.css', 'color', 'rgb(64, 76, 237)');
+    
+            cy.get('.user-info__title').click();
+            cy.get('.btn__list-item').contains('Log Out').click();
 
-        cy.get('.main-nav__link--active').contains('Time Logging');
-        cy.get('.main-nav__link--active').should('have.css', 'color', 'rgb(64, 76, 237)');
-
-        cy.get('.user-info__title').click();
-        cy.get('.btn__list-item').contains('Log Out').click();
-    })
+        })
+        })
 })
